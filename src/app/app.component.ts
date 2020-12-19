5caca3ee05143f8dc6ab9247d1e1dcaec16d2b91
Pratik Jagade <pratik.21810767@viit.ac.in>
2020-12-19 22:53:17 +0530

    Added forms module and installed bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'addition-project';
+  num1:number=2;
+  num2:number=4;
+  total:number=0;
+  constructor(){}
+
+  add():void{
+    this.total=this.num1+this.num2;
+  }
+
+
+
 }
